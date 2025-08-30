0c6da1329185dcab048bb2f08ee8021ceb0509d8
joao <jcoliveiras23@gmail.com>
2025-08-30 11:34:49 -0300

    mysql to postgre render

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,14 +12,13 @@ import { QuizzModule } from './quizz/quizz.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
-      port: Number(process.env.DB_PORT) || 3306,
-      username: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || 'password',
-      database: process.env.DB_NAME || 'quiz_app',
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false, // necessário no Render
+      },
     }),
     AuthModule,
     UserModule,
